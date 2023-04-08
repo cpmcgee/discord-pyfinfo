ae83afec5a6c2b29d6159cc5e33f4e9c4af976cc
chris <cpmcgee95@gmail.com>
2023-04-07 19:02:37 -0500

    add chat gpt functionality

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-PyFInfo
+# PyFInfo  
+This is a simple discord bot powered by Yahoo Finance and ChatGPT.  
+  
+Currently, this provides simple ticker info and a chat interface. Future plans consist of ChatGPT generated responses which provide insight to requested market data.  
 
-Get stock info:
-$ticker GME
+## Getting Started  
+- Install python3 https://www.python.org/downloads/
+- Install dependencies
+	- `pip install discord.py`
+	- `pip install yfinance`
+	- `pip install openai`
+- Add bot to your server: https://discordpy.readthedocs.io/en/stable/discord.html
+	- Make sure to save the token generated during this process
+- Generate an OpenAI API Key https://platform.openai.com/account/api-keys
+- Populate config.json with these values
+- Run the bot
+	- `python main.py`
 
-Get crypto info:
-$ticker BTC-USD
+## Example Usage  
+Get stock market info:  
+>$ticker SPY
+
+Get crypto info (USD or crypto denominated):  
+>$ticker BTC-USD  
+>$ticker ETH-BTC  
+  
+ChatGPT query:  
+>$chat What is bitcoin?  
+>$chat How is Earnings Per Share calculated?
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import json
 import discord
 import yfinance
-import textwrap
+import openai
 
+# Create discord client
 intents = discord.Intents.default()
 intents.message_content = True
+
 client = discord.Client(intents=intents)
+
+# Read in config
 config = json.load(open('config.json'))
 discord_token = config["discord_token"]
+openai.api_key = config["openai_api_key"]
+
 
 @client.event
 async def on_ready():
@@ -18,11 +24,16 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('$ticker '):
+    elif len(message.content) > 100:
+        await message.channel.send("idk")
+    elif message.content.startswith('$ticker '):
         ticker = message.content.split('$ticker ', 1)[1]
         info = get_ticker_info(ticker)
         await message.channel.send(info)
+    elif message.content.startswith('$chat '):
+        query = message.content.split('$chat ', 1)[1]
+        response = get_chat_response(query)
+        await message.channel.send(response)
 
 
 def get_ticker_info(ticker):
@@ -40,7 +51,7 @@ def get_ticker_info(ticker):
         Low:   {low}
         """
 
-        return textwrap.dedent(res)
+        return res
     except KeyError:
         return 'Cannot find ticker: ' + ticker.upper()
 
@@ -58,4 +69,15 @@ def format_prices(ticker, last, open, high, low):
     return last, open, high, low
 
 
-client.run(discord_token)
+def get_chat_response(query):
+    try:
+        system_msg = {"role": "system", "content": "You must respond in less than 35 words"}
+        user_msg = {"role": "user", "content": query}
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", max_tokens=50, messages=[system_msg, user_msg])
+        return response.choices[0].message.content
+    except:
+        return "idk"
+
+
+if __name__ == "__main__":
+    client.run(discord_token)
