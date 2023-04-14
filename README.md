4354f6b567f5fbefc565bce2301b5a2074957dcc
chris <cpmcgee95@gmail.com>
2023-04-13 20:36:00 -0500

    cleanup, refactoring, password protec

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PyFInfo  
 This is a simple discord bot powered by Yahoo Finance and ChatGPT.  
   
-Currently, this provides simple ticker info, news and a chat interface. Future plans consist of ChatGPT generated responses which provide insight to requested market data.  
+Currently, this provides simple ticker info, news and a chat interface. Future plans consist of ChatGPT generated responses which provide simple interpretations of market data.  
 
 ## Getting Started  
 - Install python3 https://www.python.org/downloads/
@@ -10,12 +10,23 @@ Currently, this provides simple ticker info, news and a chat interface. Future p
 - Add bot to your discord server: https://discordpy.readthedocs.io/en/stable/discord.html
 	- Make sure to save the token generated during this process
 - Generate an OpenAI API Key https://platform.openai.com/account/api-keys
-- Populate config.json with these values
-- Run the bot
+- Populate config.json
+	- See Config section below
+- Run the application
 	- `flask run`
+- Visit the flask server URL once to start the bot
+
+## Config
+- discord_token - the token used to log the bot into discord
+- openai_api_key - api key for accessing chat gpt
+- max_bot_query_length - character limit that defines how long of a message the bot will respond to
+- max_chat_response_words - max number of words for a generated chat response to contain
+- num_news_articles - number of recent news articles to return with $news command
+- custom_prompt - custom instructions to be included in each chat gpt query
+- ui_password - password used to access the settings UI
 
 ## Example Usage  
-Get stock market info:  
+Get stock market info:
 >$ticker SPY
 
 Get crypto info (USD or crypto denominated):  
@@ -28,3 +39,11 @@ Get latest news articles (this is buggy with crypto due to pyfinance limitations
 ChatGPT query:  
 >$chat What is bitcoin?  
 >$chat How is Earnings Per Share calculated?
+
+## Settings UI
+The settings UI can be accessed by visiting the url of the flask server and providing a `password` query parameter.
+
+Example:
+`http://localhost:5000?password=secret`
+
+![Alt text](/static/settings_ui.jpg?raw=true)
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import main
+import listener
 from flask import Flask, render_template, request
 import threading
 import json
@@ -8,29 +8,38 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        config['max_bot_query_length'] = int(request.form['max_bot_query_length']) or config['max_bot_query_length']
-        config['max_chat_response_words'] = int(request.form['max_chat_response_words']) or config['max_chat_response_words']
-        config['num_news_articles'] = int(request.form['num_news_articles']) or config['num_news_articles']
-        config['custom_prompt'] = str(request.form['custom_prompt'])
-    return render_template('index.html', **config)
+    pass_attempt = request.args.get('password')
+    pass_actual = config['ui_password']
+
+    if pass_attempt == pass_actual:
+        if request.method == 'POST':
+            config['max_bot_query_length'] = int(request.form['max_bot_query_length']) or config['max_bot_query_length']
+            config['max_chat_response_words'] = int(request.form['max_chat_response_words']) or config['max_chat_response_words']
+            config['num_news_articles'] = int(request.form['num_news_articles']) or config['num_news_articles']
+            config['custom_prompt'] = str(request.form['custom_prompt'])
+            print('Settings updated')
+        return render_template('settings.html', **config)
+    else:
+        return render_template('index.html')
+
 
 def load_config():
     config = json.load(open('config.json'))
 
     return {
-        'discord_token': config['discord_token'],                       #the discord app token used to join a server
-        'openai_api_key': config['openai_api_key'],                     #custom instructions to be included in each chat gpt query
-        'max_bot_query_length': config['max_bot_query_length'],         #character limit that defines how long of a message the bot will respond to
-        'max_chat_response_words': config['max_chat_response_words'],   #max number of words for a generated chat response to contain
-        'num_news_articles': config['num_news_articles'],               #number of recent news articles to return with $news command
-        'custom_prompt': config['custom_prompt']                        #custom instructions to be included in each chat gpt query
+        'discord_token': config['discord_token'],
+        'openai_api_key': config['openai_api_key'],
+        'max_bot_query_length': config['max_bot_query_length'],
+        'max_chat_response_words': config['max_chat_response_words'],
+        'num_news_articles': config['num_news_articles'],
+        'custom_prompt': config['custom_prompt'],
+        'ui_password': config['ui_password']
     }
 
 @app.before_first_request
 def setup_background_task():
     global config
     config = load_config()
-    background_thread = threading.Thread(target=lambda: main.start(config))
+    background_thread = threading.Thread(target=lambda: listener.start(config))
     background_thread.daemon = False
     background_thread.start()
